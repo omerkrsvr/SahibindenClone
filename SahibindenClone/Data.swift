dba3ed53e575fbcfdb976ac99a6cb7bfb81e2ca6
omerkrsvr <omerkirsever@gmail.com>
2021-12-08 23:06:09 +0300

    added categories data

---
+++ SahibindenClone/Data.swift
@@ -0,0 +1,23 @@
+//
+//  Data.swift
+//  SahibindenClone
+//
+//  Created by Omer Kırsever on 8.12.2021.
+//
+
+import Foundation
+
+var categories: [Category] = [
+    
+        Category(categoryName: "Emlak", categoryInfo: "Konut, İş Yeri, Arsa, Projeler, Bina, Devre Mülk, Turistik Tesis", categoryImage: "emlak"),
+        Category(categoryName: "Vasıta", categoryInfo: "Otomobil, Arazi, SUV & Pickup, Motosiklet, Minivan, Kamyonet", categoryImage: "vasita"),
+        Category(categoryName: "Yedek Parça, Aksesuar, Donanım & Tuning ", categoryInfo: "Otomotiv Ekipmanları, Motosiklet Ekipmanları, Deniz Aracı Ekipmanları", categoryImage: "yedekparca"),
+        Category(categoryName: "İkinci el ve Sıfır Alışveriş", categoryInfo: "Bilgisayar, Cep Telefonu, Fotoğraf & Kamera, Ev Dekorasyon", categoryImage: "alisveris"),
+        Category(categoryName: "İş Makineleri & Sanayi", categoryInfo: "İş Makineleri, Tarım Makineleri, Sanayi, Elektrik & Enerji", categoryImage: "ismakine"),
+        Category(categoryName: "Ustalar ve Hizmetler", categoryInfo: "Ev Tadilat, Dekorasyon, Nakliye, Araç Servis & Bakım", categoryImage: "hizmet"),
+        Category(categoryName: "Özel Ders Verenler", categoryInfo: "Lise & Üniversite, İlkokul & Ortaokul, Yabancı Dil, Bilgisayar", categoryImage: "kitap"),
+        Category(categoryName: "İş İlanları", categoryInfo: "Avukatlık & Hukuki Danışmanlık, Eğitim, Eğlence ve Aktiviteler, Güzellik ve Bakım", categoryImage: "isilan"),
+        Category(categoryName: "Yardımcı Arayanlar", categoryInfo: "Bebek & Çocuk Bakıcısı, Yaşlı & Hasta Bakıcısı, Temizlikçi & Ev İşlerine Yardımcı", categoryImage: "yardimci"),
+        Category(categoryName: "Hayvanlar Alemi", categoryInfo: "Evcil Hayvanlar, Akvaryum Balıkları, Aksesuarlar, Bakım Ürünleri, Yem & Mama", categoryImage: "hayvanlar")
+        
+]
