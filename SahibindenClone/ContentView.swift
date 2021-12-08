747e199923354448806354fec9dc96c8c0e547a7
omerkrsvr <omerkirsever@gmail.com>
2021-12-08 21:28:12 +0300

    Initial Commit

---
+++ SahibindenClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SahibindenClone
+//
+//  Created by Omer Kırsever on 8.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SahibindenClone/SahibindenCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahibindenCloneApp.swift
+//  SahibindenClone
+//
+//  Created by Omer Kırsever on 8.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SahibindenCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
